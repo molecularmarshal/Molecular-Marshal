943a317b366e7ffab57f90de852899d8e9a4e311
chaozheng <charleszheng44@gmail.com>
2014-06-23 15:19:17 +0800

    delete log file

--- apps/pi_mc/scripts/test.py
+++
@@ -1,9 +0,0 @@
-import generator
-
-if __name__ == '__main__':
-
-  with open('gen_config.txt', 'r') as ifp:
-    d = eval(ifp.read())
-
-  generator.Generator.parse_gen_opts(d['generators'])
-  print generator.Generator.generator_options  
